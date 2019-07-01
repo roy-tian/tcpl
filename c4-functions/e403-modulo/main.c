6e383022ebffcc3d26489de0f7c8d44773bf83fd
tdjol@hotmail.com <Tc3k8-yb46t>
2019-07-02 01:57:34 +0800

    update

--- c4-functions/e403-modulo/main.c
+++ c4-functions/e403-modulo/main.c
@@ -1,6 +1,9 @@
 #include "shell.h"
 
+void open_file(RoyDeque * parsed_line);
+
 int main(void) {
   RoyShell * shell = roy_shell_new();
+  roy_shell_add_command(shell, "open", ROY_OPERATE(open_file));
   roy_shell_start(shell);
 }
