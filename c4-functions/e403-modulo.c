f84627cb5ddb155c3974f79cafb13483ec3ecf70
Roy Tian <tdjol@hotmail.com>
2019-07-10 18:02:19 +0800

    update

--- c4-functions/e403-modulo.c
+++ c4-functions/e403-modulo.c
@@ -1,9 +1,37 @@
 #include "../../roylib/roy.h"
 
-void open_file(RoyDeque * parsed_line);
+char content[STRING_CAPACITY_HUGE] = "\0";
+
+void open(RoyDeque * deque) {
+  const char * name = roy_deque_at(deque, char, 1);
+  FILE * fp = fopen(name, "r");
+  if (fp) {
+    fseek(fp, 0, SEEK_END);
+    size_t size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    fread(content, sizeof(char), size, fp);
+    printf("%s open successfully.\n", name);
+  } else {
+    puts("file not found.");
+  }
+}
+
+void show(RoyDeque * deque) {
+  if (content) {
+    puts(content);
+  } else {
+    puts("nothing to show.");
+  }
+}
+
+void quit(RoyDeque * deque) {
+  exit(EXIT_SUCCESS);
+}
 
 int main(void) {
   RoyShell * shell = roy_shell_new();
-  roy_shell_add_command(shell, "open", ROY_OPERATE(open_file));
+  roy_shell_add_command(shell, "open", open);
+  roy_shell_add_command(shell, "show", show);
+  roy_shell_add_command(shell, "quit", quit);
   roy_shell_start(shell);
 }
