e4e8cf1eed5122744ba2d43e5ed1c8e43f25e8b5
roy-tian <tdjol@hotmail.com>
2019-08-12 20:20:56 +0800

    rename & revise

--- c5-pointers/e515_sorti.c
+++ c5-pointers/e515_sorti.c
@@ -6,6 +6,7 @@ RoyVector * elements;
 enum {
   VECTOR_START_CAPACITY = 10,
   STRING_CAPACITY = 255,
+  SORT_STRING = 0,
   SORT_NUMBER = 1,
   SORT_REVERSELY = 1 << 1,
   SORT_INSENSITIVELY = 1 << 2,
@@ -16,10 +17,10 @@ void read(RoyShell * shell);
 void select(RoyShell * shell);
 void sort(RoyShell * shell);
 void quit(RoyShell * shell);
-void printDouble(const char * str);
-void printStr(const char * str);
+void print(const char * str);
 int numcmp(const char * str1, const char * str2);
 int numcmpr(const char * str1, const char * str2);
+int strcmpi(const char * str1, const char * str2);
 int strcmpr(const char * str1, const char * str2);
 int strcmpri(const char * str1, const char * str2);
 
@@ -32,6 +33,10 @@ void insert(RoyShell * shell) {
 
 void read(RoyShell * shell) {
   FILE * fp = fopen(roy_shell_argument_at(shell, 1), "r");
+  if (!fp) {
+    printf("File '%s' open error.\n", roy_shell_argument_at(shell, 1));
+    return;
+  }
   ROY_STR(buf, STRING_CAPACITY)
   while (fgets(buf, sizeof buf, fp)) {
     if (*(buf + strlen(buf) - 1) == '\n') {
@@ -40,14 +45,11 @@ void read(RoyShell * shell) {
     roy_vector_push_back(elements, buf);
   }
   fclose(fp);
+  printf("%zu lines added.\n", roy_vector_size(elements));
 }
 
 void select(RoyShell * shell) {
-  if (roy_shell_argument_find(shell, "-n|--number") != -1) {
-    roy_vector_for_each(elements, ROY_OPERATE(printDouble));
-  } else {
-    roy_vector_for_each(elements, ROY_OPERATE(printStr));
-  }
+  roy_vector_for_each(elements, ROY_OPERATE(print));
 }
 
 void sort(RoyShell * shell) {
@@ -67,16 +69,16 @@ void sort(RoyShell * shell) {
   case SORT_NUMBER | SORT_REVERSELY:
     comp = ROY_COMPARE(numcmpr);
     break;
-  case SORT_REVERSELY:
+  case SORT_STRING | SORT_REVERSELY:
     comp = ROY_COMPARE(strcmpr);
     break;
-  case SORT_INSENSITIVELY:
+  case SORT_STRING | SORT_INSENSITIVELY:
     comp = ROY_COMPARE(strcmpi);
     break;
-  case SORT_REVERSELY | SORT_INSENSITIVELY:
+  case SORT_STRING | SORT_REVERSELY | SORT_INSENSITIVELY:
     comp = ROY_COMPARE(strcmpri);
     break;
-  case 0:
+  case SORT_STRING:
     comp = ROY_COMPARE(strcmp);
     break;
   default:
@@ -92,14 +94,10 @@ void sort(RoyShell * shell) {
 void quit(RoyShell * shell) {
   roy_vector_delete(elements);
   roy_shell_delete(shell);
-  quit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
 
-void printDouble(const char  * str) {
-  printf("%.16g\n", atof(str));
-}
-
-void printStr(const char * str) {
+void print(const char * str) {
   puts(str);
 }
 
@@ -113,6 +111,23 @@ int numcmpr(const char * str1, const char * str2) {
   return -numcmp(str1, str2);
 }
 
+char * str_tolower(char * str) {
+  char * pstr = str;
+  while (*pstr != '\0') {
+    *pstr = tolower(*pstr);
+    pstr++;
+  }
+  return str;
+}
+
+int strcmpi(const char * str1, const char * str2) {
+  ROY_STR(str1u, strlen(str1))
+  strcpy(str1u, str1);
+  ROY_STR(str2u, strlen(str2))
+  strcpy(str2u, str2);
+  return strcmp(str_tolower(str1u), str_tolower(str2u));
+}
+
 int strcmpr(const char * str1, const char * str2) {
   return -strcmp(str1, str2);
 }
@@ -124,7 +139,7 @@ int strcmpri(const char * str1, const char * str2) {
 int main(void) {
   shell = roy_shell_new();
   elements = roy_vector_new(VECTOR_START_CAPACITY,
-                            sizeof(char ) * (STRING_CAPACITY + 1));
+                            sizeof(char) * (STRING_CAPACITY + 1));
   roy_shell_command_add(shell, "insert", insert);
   roy_shell_command_add(shell, "read", read);
   roy_shell_command_add(shell, "select", select);
