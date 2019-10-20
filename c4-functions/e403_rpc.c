793b5119535f2eb9a69069d489f397dc2b2d5de6
田德健 <roy@tiandejiandeMacBook-Pro.local>
2019-10-20 23:09:43 +0800

    rename variables

--- c4-functions/e403_rpc.c
+++ c4-functions/e403_rpc.c
@@ -4,43 +4,43 @@
 
 #include "roy.h"
 
-void rpc(RoyShell * shell);
-void quit(RoyShell * shell);
+void rpc(RoyShell *);
+void quit(RoyShell *);
 
-bool validNumber(const RoyString * arg);
-bool validOperator(const RoyString * arg);
-void doNumber(RoyStack * operands, RoyString * arg);
-void doOperate(RoyStack * operands, RoyString * arg);
-void doError(RoyStack * operands, RoyString * arg, const char * errInfo);
-double operate(double operand1, double operand2, int operator_);
+bool validNumber(const RoyString *);
+bool validOperator(const RoyString *);
+void doNumber(RoyStack *, const RoyString *);
+void doOperate(RoyStack *, const RoyString *);
+void doError(RoyStack *, RoyString *, const char *);
+double operate(double, double, int);
 
 void rpc(RoyShell * shell) {
   enum { CAPACITY = 128 };
-  RoyStack * operands = roy_stack_new(CAPACITY, sizeof(double));
-  RoyString * arg = roy_string_new();
+  RoyStack * tokens = roy_stack_new(CAPACITY, sizeof(double));
+  RoyString * token = roy_string_new();
   for (size_t i = 1; i != roy_shell_argument_count(shell); i++) {
-    roy_string_assign(arg, roy_shell_argument_at(shell, i));
-    if (validNumber(arg)) {
-      doNumber(operands, arg);
-    } else if (validOperator(arg) && roy_stack_size(operands) >= 2) { // enough operands
-      doOperate(operands, arg);
-    } else if (validOperator(arg) && roy_stack_size(operands) < 2) {
-      doError(operands, arg, "operands not enough.");
+    roy_string_assign(token, roy_shell_argument_at(shell, i));
+    if (validNumber(token)) {
+      doNumber(tokens, token);
+    } else if (validOperator(token) && roy_stack_size(tokens) >= 2) { // enough tokenStack
+      doOperate(tokens, token);
+    } else if (validOperator(token) && roy_stack_size(tokens) < 2) {
+      doError(tokens, token, "tokens not enough.");
       return;
     } else {
-      roy_string_prepend_str(arg, "unrecognised token - \'");
-      roy_string_append_str(arg, "\'.");
-      doError(operands, arg, roy_string_cstr(arg));
+      roy_string_prepend_str(token, "unrecognised token - \'");
+      roy_string_append_str(token, "\'.");
+      doError(tokens, token, roy_string_cstr(token));
       return; 
     }
   }
-  if (roy_stack_size(operands) > 1) {
-    doError(operands, arg, "operands not empty.");
+  if (roy_stack_size(tokens) > 1) {
+    doError(tokens, token, "parsing ends but stack not empty.");
     return;
   }
-  printf("%.16g\n", *roy_stack_top(operands, double));
-  roy_string_delete(arg);
-  roy_stack_delete(operands);
+  printf("%.16g\n", *roy_stack_top(tokens, double));
+  roy_string_delete(token);
+  roy_stack_delete(tokens);
 }
 
 void quit(RoyShell * shell) {
@@ -49,40 +49,40 @@ void quit(RoyShell * shell) {
 }
 
 bool
-validNumber(const RoyString * arg) {
-  return roy_string_match(arg, "[\\+\\-]?(\\d+\\.?\\d*|\\d*\\.?\\d+)([Ee][\\+\\-]?\\d+)?");
+validNumber(const RoyString * token) {
+  return roy_string_match(token, "[\\+\\-]?(\\d+\\.?\\d*|\\d*\\.?\\d+)([Ee][\\+\\-]?\\d+)?");
 }
 
 bool
-validOperator(const RoyString * arg) {
-  return roy_string_match(arg, "[\\+\\-\\*/%]");
+validOperator(const RoyString * token) {
+  return roy_string_match(token, "[\\+\\-\\*/%]");
 }
 
 void
-doNumber(RoyStack  * operands,
-         RoyString * arg) {
-  double value = atof(roy_string_cstr(arg));
-  roy_stack_push(operands, &value);
+doNumber(RoyStack        * tokenStack,
+         const RoyString * token) {
+  double value = atof(roy_string_cstr(token));
+  roy_stack_push(tokenStack, &value);
 }
 
 void
-doOperate(RoyStack  * operands,
-          RoyString * arg) {
-  double operand1 = *roy_stack_top(operands, double);
-  roy_stack_pop(operands);
-  double operand2 = *roy_stack_top(operands, double);
-  roy_stack_pop(operands);
-  double result = operate(operand2, operand1, roy_string_at(arg, 0));
-  roy_stack_push(operands, &result);
+doOperate(RoyStack        * tokenStack,
+          const RoyString * token) {
+  double operand1 = *roy_stack_top(tokenStack, double);
+  roy_stack_pop(tokenStack);
+  double operand2 = *roy_stack_top(tokenStack, double);
+  roy_stack_pop(tokenStack);
+  double result = operate(operand2, operand1, roy_string_at(token, 0));
+  roy_stack_push(tokenStack, &result);
 }
 
 void
-doError(RoyStack   * operands,
-        RoyString  * arg,
+doError(RoyStack   * tokenStack,
+        RoyString  * token,
         const char * errInfo) {
   printf("Syntax error: %s\n", errInfo);
-  roy_string_delete(arg);
-  roy_stack_delete(operands);
+  roy_string_delete(token);
+  roy_stack_delete(tokenStack);
 }
 
 double
