7dec4ab85864a46904a8f27b459da5c156d451b1
roy-tian <tdjol@hotmail.com>
2019-11-11 14:53:05 +0800

    remove e406 folder

--- c4-functions/e406_var/calc.h
+++
@@ -1,5 +0,0 @@
-typedef double (* UnaryOperator)(double);
-typedef double (* BinaryOperator)(double, double);
-
-UnaryOperator strToUnaryOperator(const char *);
-BinaryOperator strToBinaryOperator(const char *);
--- c4-functions/e406_var/shell.h
+++
@@ -1,9 +0,0 @@
-#include "roy.h"
-#include "calc.h"
-
-extern RoyShell * shell;
-extern RoyStack * operands;
-extern RoyMap * variables;
-
-void rpc(RoyShell *);
-void quit(RoyShell *);
