1b73010058915eda6300c9e80912ba0ad4d2edd8
roy-tian <tdjol@hotmail.com>
2020-01-08 18:07:20 +0800

    e107: add '\n'

--- c1-tutorial/e107_eof.c
+++ c1-tutorial/e107_eof.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("The value of EOF is %d.", EOF);
+  printf("The value of EOF is %d.\n", EOF);
   // integer constant expression of type int and negative value.
 }
