e5893b66338490dd0f0e4476d676fe478501c2f8
roy-tian <tdjol@hotmail.com>
2020-02-18 19:03:59 +0800

    upload e404

--- c4-functions/e404_stack.c
+++ c4-functions/e404_stack.c
@@ -1,17 +1,17 @@
-#include "stack.h"
+#include "roy.h"
+
+RoyStack * stack;
 
 void push(RoyShell * shell) {
   for (size_t i = 1; i != roy_shell_argc(shell); i++) {
-    Double * temp =
-      doubleNew(roy_string_to_double((roy_shell_argv_at(shell, i))));
-    roy_stack_push(stack, temp);
+    roy_stack_push(stack, roy_string_copy(roy_shell_argv_at(shell, i)));
   }
   printf("%zu element(s) pushed.\n", roy_shell_argc(shell) - 1);
 }
 
 void peek(RoyShell * shell) {
   if (!roy_stack_empty(stack)) {
-    doublePrintln(roy_stack_top(stack, Double));
+    roy_string_println(roy_stack_top(stack, RoyString));
   } else {
     puts("Peek failed: stack is empty.");
   }
@@ -27,11 +27,11 @@ void pop(RoyShell * shell) {
     return;
   }
   if (count > roy_stack_size(stack)) {
-    printf("Warning: count (%d) is larger than the size of stack(%zu).",
+    printf("Warning: count (%d) is oversized (%zu), this will only clear the stack.\n",
            count, roy_stack_size(stack));
     count = roy_stack_size(stack);
   }
-  for (size_t i = 0; i != count; i++) {
+  for (int i = 0; i != count; i++) {
     roy_stack_pop(stack);
   }
   printf("%d element(s) popped.\n", count);
@@ -39,9 +39,9 @@ void pop(RoyShell * shell) {
 
 void swap(RoyShell * shell) {
   if (roy_stack_size(stack) >= 2) {
-    Double * top1 = doubleCopy(roy_stack_top(stack, Double));
+    RoyString * top1 = roy_string_copy(roy_stack_top(stack, RoyString));
     roy_stack_pop(stack);
-    Double * top2 = doubleCopy(roy_stack_top(stack, Double));
+    RoyString * top2 = roy_string_copy(roy_stack_top(stack, RoyString));
     roy_stack_pop(stack);
     roy_stack_push(stack, top1);
     roy_stack_push(stack, top2);
@@ -52,9 +52,7 @@ void swap(RoyShell * shell) {
 }
 
 void clear(RoyShell * shell) {
-  while (!roy_stack_empty(stack)) {
-    roy_stack_pop(stack);
-  }
+  roy_stack_clear(stack);
   puts("All elements cleared.");
 }
 
@@ -62,4 +60,16 @@ void quit(RoyShell * shell) {
   roy_stack_delete(stack);
   roy_shell_delete(shell);
   exit(EXIT_SUCCESS);
+}
+
+int main(void) {
+  stack = roy_stack_new(R_BUF_SIZE, (ROperate)roy_string_delete);
+  RoyShell * shell = roy_shell_new();
+  roy_shell_add(shell, push);
+  roy_shell_add(shell, peek);
+  roy_shell_add(shell, pop);
+  roy_shell_add(shell, swap);
+  roy_shell_add(shell, clear);
+  roy_shell_add(shell, quit);
+  roy_shell_start(shell);
 }
