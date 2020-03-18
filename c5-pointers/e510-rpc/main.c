8968fd0a4001f5c7555aea4fca84a386824bc443
roy-tian <tdjol@hotmail.com>
2020-03-18 16:50:23 +0800

    solve the bug with 0 arg

--- c5-pointers/e510-rpc/main.c
+++ c5-pointers/e510-rpc/main.c
@@ -5,11 +5,14 @@
 #include "rpc.h"
 
 int main(int argc, char ** argv) {
-  populate();
-  RoyDeque * args = roy_deque_new((ROperate)roy_string_delete);
-  for (int i = 1; i != argc; i++) {
-    roy_deque_push_back(args, roy_string_new(argv[i]));
+  if (argc > 1) {
+    populate();
+    RoyDeque * args = roy_deque_new((ROperate)roy_string_delete);
+    for (int i = 1; i < argc; i++) {
+      roy_deque_push_back(args, roy_string_new(argv[i]));
+    }
+    rpc(args);
+    roy_deque_delete(args);
+    roy_map_delete(operators);
   }
-  rpc(args);
-  roy_deque_delete(args);
 }
