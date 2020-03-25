834cb6ef9849020b92c022b58e27c414b925b0a6
roy-tian <tdjol@hotmail.com>
2020-03-26 07:45:20 +0800

    upload e513

--- c5-pointers/e513_tail.c
+++ c5-pointers/e513_tail.c
@@ -1,41 +1,50 @@
 #include "roy.h"
 
-const char * fileName(int argc, char ** argv) {
-  if (argc == 2 && *argv[1] != '-') {
-    return (const char *)argv[1];
+int tail(const char * arg) {
+  if (*arg != '-') {
+    return -1;
   }
-  if (argc == 3) {
-    if (*argv[1] == '-' && *argv[2] != '-') {
-      return (const char*)argv[2];
-    }
-    if (*argv[1] != '-' && *argv[2] == '-') {
-      return (const char*)argv[1];
-    }
+  char * end;
+  int ret = strtol(arg + 1, &end, 0);
+  if (arg + 1 == end) {
+    return -1;
   }
-  return NULL;
+  return ret;
 }
 
-int ntail(int argc, char ** argv) {
-  if (argc == 2 && *argv[1] != '-') {
-    return 10;
+bool parseArgs(int argc, char ** argv, const char ** fileName, int * ntail) {
+  if (argc == 2 && argv[1][0] != '-') {
+    *fileName = argv[1];
+    *ntail = 10;
   }
-  if (argc == 3) {
-    if (*argv[1] == '-') {
-      return (int)strtol(argv[1] + 1, NULL, 0);
-    }
-    if (*argv[2] == '-') {
-      return (int)strtol(argv[2] + 1, NULL, 0);
-    }
+  int ret = 0;
+  if (argc == 3 && (ret = tail(argv[1])) != -1) {
+    *fileName = argv[2];
+    *ntail = ret;
   }
-  return 0;
+  if (argc == 3 && (ret = tail(argv[2])) != -1) {
+    *fileName = argv[1];
+    *ntail = ret;
+  }
+  return *fileName;
 }
 
 int main(int argc, char ** argv) {
-  RoyString * str = roy_string_read_file(fileName(argc, argv));
+  const char * fileName = NULL;
+  int ntail = 0;
+  if ( !parseArgs(argc, argv, &fileName, &ntail) ) {
+    perror(fileName);
+    exit(EXIT_FAILURE);
+  }
+  RoyString * str = roy_string_read_file(fileName);
   RoyDeque * deque = roy_deque_new((ROperate)roy_string_delete);
   roy_string_split(deque, str, "\n");
+
   size_t lineCount = roy_deque_size(deque);
-  for (int i = lineCount - ntail(argc, argv); i != lineCount; i++) {
+  if (ntail > lineCount) {
+    ntail = lineCount;
+  }
+  for (int i = lineCount - ntail; i != lineCount; i++) {
     roy_string_println(roy_deque_at(deque, i, RoyString));
   }
 }
